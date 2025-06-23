3cf7ce52d2dc9115ed146f06d58f11728c65ead3
Aman Gupta <aman07112006@gmail.com>
2025-06-23 20:52:04 +0530

    make cahnges in setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ def get_requirements(filename):
         return [line.strip() for line in f if line.strip() and not line.startswith(('#','-'))]
 
 setup(
-    name=APP_NAME, 
-    author='DeadlyHarbor', 
+    name=APP_NAME,
+    author='DeadlyHarbor',
     author_email='aman07112006@gmail.com',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=get_requirements('requirements.txt')
 )
